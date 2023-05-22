433b729bff333b50a24d6cb80b0e000bccb0a85a
torekhanmukhtarov <nahkerot@gmail.com>
2023-05-22 11:19:58 +0600

    added host

--- generated/core/OpenAPI.ts
+++ generated/core/OpenAPI.ts
@@ -21,7 +21,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-  BASE: "http://localhost:3000",
+  BASE: window.location.hostname === "localhost" ? "http://localhost:3000" : "https://164.92.180.13:3000",
   VERSION: "1.0",
   WITH_CREDENTIALS: false,
   CREDENTIALS: "include",
